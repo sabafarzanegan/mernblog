ee3fcc0e632b33922e27b1b547e1e466ec301238
saba farzanegan <saba.far.99@gmail.com>
2024-06-10 22:16:05 +0330

    create new navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
+
 import About from "./pages/About";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
+import Header from "./components/Navbar";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        <Header />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/profile" element={<Profile />} />
+
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
         </Routes>
